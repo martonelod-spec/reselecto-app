e76297ad2e713cbf8a53f8d36faa87fdeec404bb
martonelod-spec <martonelod@gmail.com>
2026-03-28 13:27:26 +0200

    Add app/config/sms.js

---
+++ app/config/sms.js
@@ -0,0 +1,9 @@
+// ============================================================
+// SMS sablon konfiguráció
+// ============================================================
+
+export const SMS_CONFIG = {
+  // Alapértelmezett SMS szöveg (ha a hívás nem fogadott)
+  defaultMessage:
+    'Tisztelt Ügyfelünk! A ReSelecto futárszolgálata kereste Önt a szállítással kapcsolatban. Kérem hívjon vissza. Köszönjük!',
+};
